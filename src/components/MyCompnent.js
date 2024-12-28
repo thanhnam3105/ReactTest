96df10adb92ed453d7bf67a51c99055d87a07758
thanhnam3105 <phungthanhnam3105@gmail.com>
2024-12-28 20:27:48 +0700

    28/12

---
+++ src/components/MyCompnent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Mycomponent extends React.Component{
+    render(){
+        return(
+            <div>thanh nam 2ssssssss</div>
+        );
+    }
+}
+
+export default Mycomponent;
