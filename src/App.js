af9b4bc1f8c87e7a10bc91ed2e0bd89204febb14
thanhnam3105 <phungthanhnam3105@gmail.com>
2024-12-28 21:29:40 +0700

    commit lan 2 - 28/12

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
-import Mycomponent from './components/MyCompnent';
-
-function App() {
-  return (
-    <div>
-
-      <Mycomponent></Mycomponent>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+import Mycomponent from "./components/MyCompnent";
+import React from "react";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Mycomponent></Mycomponent>
+      </div>
+    );
+  }
 }
+// function App() {
+//   return (
+
+//     // <div className="App">
+//     //   <header className="App-header">
+//     //     <img src={logo} className="App-logo" alt="logo" />
+//     //     <p>
+//     //       Edit <code>src/App.js</code> and save to reload.
+//     //     </p>
+//     //     <a
+//     //       className="App-link"
+//     //       href="https://reactjs.org"
+//     //       target="_blank"
+//     //       rel="noopener noreferrer"
+//     //     >
+//     //       Learn React
+//     //     </a>
+//     //   </header>
+//     // </div>
+//   );
+// }
 
 export default App;
--- src/components/MyCompnent.js
+++ src/components/MyCompnent.js
@@ -1,11 +1,13 @@
 import React from "react";
 
-class Mycomponent extends React.Component{
-    render(){
-        return(
-            <div>thanh nam 2ssssssss</div>
-        );
-    }
+class Mycomponent extends React.Component {
+  state = {
+    name: "nam",
+    diaChi: "tphcm",
+  };
+  render() {
+    return <div>{this.state.diaChi}</div>;
+  }
 }
 
 export default Mycomponent;
